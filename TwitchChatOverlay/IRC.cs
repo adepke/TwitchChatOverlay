e0218529483992f4af1bbdb7eba89e12197765c7
XenonicDev <andrewdepke@gmail.com>
2018-06-25 15:58:00 -0600

    IRC API Wrapper, Setting up Main and Window Thread

---
+++ TwitchChatOverlay/IRC.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TwitchChatOverlay
+{
+    class IRC
+    {
+        private TcpClient TCPHandle;
+        private StreamReader InStream;
+        private StreamWriter OutStream;
+
+        private string Username;
+        private string CurrentChannel;
+
+        public void Boot(string HostName, int Port, string InUsername, string OAuthToken)
+        {
+            Username = InUsername;
+
+            TCPHandle = new TcpClient(HostName, Port);
+            InStream = new StreamReader(TCPHandle.GetStream());
+            OutStream = new StreamWriter(TCPHandle.GetStream());
+
+            SendToIRC("PASS " + OAuthToken);
+            SendToIRC("NICK " + Username);
+            SendToIRC("USER " + Username);
+        }
+
+        public void SendToIRC(string Message)
+        {
+            OutStream.WriteLine(Message);
+            OutStream.Flush();
+        }
+
+        public void JoinRoom(string Channel)
+        {
+            SendToIRC("JOIN #" + Channel);
+
+            CurrentChannel = Channel;
+        }
+
+        public void SendMessage(string Message)
+        {
+            SendToIRC(":" + Username + "!" + Username + "@" + Username + ".tmi.twitch.tv PRIVMSG #" + CurrentChannel + " :" + Message);
+        }
+
+        public string ReadMessage()
+        {
+            return InStream.ReadLine();
+        }
+    }
+}
---
+++ TwitchChatOverlay/TwitchOverlayBot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TwitchChatOverlay
+{
+    class TwitchOverlayBot
+    {
+        public static void Main(string[] Args)
+        {
+            Thread WindowThread = new Thread(() =>
+            {
+                MainWindow Window = new MainWindow();
+                Window.Show();
+            });
+
+            WindowThread.Start();
+
+            IRC IRCClient = new IRC();
+            IRCClient.Boot("irc.twitch.tv", 6667);
+        }
+    }
+}

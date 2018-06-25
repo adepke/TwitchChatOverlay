2a86595b42240e437d5c9510b0dc3ca3b1835277
XenonicDev <andrewdepke@gmail.com>
2018-06-25 13:47:05 -0600

    Setting Up Login Window

--- TwitchChatOverlay/MainWindow.xaml.cs
+++ TwitchChatOverlay/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TwitchChatOverlay
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

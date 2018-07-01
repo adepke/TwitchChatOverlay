c6a7393b76402acca16f3753c062aae2a50e7ed6
XenonicDev <andrewdepke@gmail.com>
2018-07-01 14:00:55 -0600

    Made Bold Button Toggleable

---
+++ TwitchChatOverlay/ToggleableRadioButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TwitchChatOverlay
+{
+    class ToggleableRadioButton : RadioButton
+    {
+        protected override void OnClick()
+        {
+            if (IsChecked == true)
+            {
+                IsChecked = false;
+            }
+
+            else
+            {
+                base.OnClick();
+            }
+        }
+    }
+}
